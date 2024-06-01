9e493eb51362550ca279d60b1b955c3e20f9b4c4
r.mcgatha <ryanmcgatha@gmail.com>
2024-06-01 07:59:56 -0400

    vercel update

--- mailer.js
+++ mailer.js
@@ -4,11 +4,10 @@ require("dotenv").config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendVerificationEmail = (email, token) => {
-  console.log(`Sending verification email to: ${email} with token: ${token}`);
   const url = `https://push-it-backend.vercel.app/verify-email?token=${token}`;
   const msg = {
     to: email,
-    from: process.env.EMAIL_USER, // Use the email address or domain you verified with SendGrid
+    from: process.env.EMAIL_USER,
     subject: "Verify your email",
     html: `<p>Click <a href="${url}">here</a> to verify your email</p>`,
   };
@@ -19,10 +18,7 @@ const sendVerificationEmail = (email, token) => {
       console.log("Email sent");
     })
     .catch((error) => {
-      console.error(
-        "Error sending email:",
-        error.response ? error.response.body : error
-      );
+      console.error("Error sending email:", error);
     });
 };
 
