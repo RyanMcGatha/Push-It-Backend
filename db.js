5f04fd2202e1b809b1d4d407b6c647d5e2e6422b
r.mcgatha <ryanmcgatha@gmail.com>
2024-06-01 17:56:09 -0400

    update

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-// Ensure necessary environment variables are set
 if (!process.env.DB_CONNECT) {
   throw new Error("Environment variable DB_CONNECT must be set");
 }
@@ -14,11 +13,10 @@ const sslConfig = {
 const pool = new Pool({
   connectionString: process.env.DB_CONNECT,
   ssl: sslConfig,
-  idleTimeoutMillis: 30000,
+  idleTimeoutMillis: 5000,
   max: 20,
 });
 
-// Add error handling for the connection pool
 pool.on("error", (err, client) => {
   console.error("Unexpected error on idle client", err);
   process.exit(-1);
