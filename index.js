ff15a8dea3767cf997b6bfea601b7f31b5da060a
r.mcgatha <ryanmcgatha@gmail.com>
2024-06-04 14:34:22 -0400

    update

--- index.js
+++ index.js
@@ -279,7 +279,7 @@ app.post(
       }
 
       const token = jwt.sign({ username: user.username }, secretKey, {
-        expiresIn: "10h",
+        expiresIn: "1s",
       });
       res.json({
         token,
